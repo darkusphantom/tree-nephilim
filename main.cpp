1ea4a05396e72770ffd5a70b1084eeb806763a5e
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2022-01-31 23:25:17 -0400

    wip

--- main.cpp
+++ main.cpp
@@ -6,83 +6,145 @@
 
 using namespace std;
 
-string nodeInitial, nodeEnding;
-list<string> linkedFinal;
-
 template <typename T>
-void getDataInitialAndEnd(list<T> &linkedList) {
-  int amountChar;
-  string dataPerson;
-  string delimiter= "-";
-  string dataInitialAndEnd[1];
-
-  string words;
-  words = linkedList.front();
-  linkedList.pop_front();
-
-  int iteratorMaxPerson=0;
-  while (((amountChar = words.find(delimiter)) != string::npos) && iteratorMaxPerson < 2) {
-    dataPerson = words.substr(0, amountChar+2);
-    words.erase(0, amountChar + delimiter.length() + 2);
-
-    dataInitialAndEnd[iteratorMaxPerson] = dataPerson;
-    iteratorMaxPerson++;
-  }
-
-  nodeInitial = dataInitialAndEnd[0];
-  nodeEnding = dataInitialAndEnd[1];
+void readData(T &data, string info) {
+  cout << "Ingrese " << info << " : " << endl;
+  cin >> data;
 }
 
-template <typename T>
-void setTree(list<T> &linkedList1, list<T> &linkedList2) {
-  ArbolBinario<string> *humanTree = new ArbolBinario<string>;
-  list<string> lista;
+void saveDataInList(list<string> &linkedList1, list<string> &linkedList2) {
+  string stringToken;
 
-  showElementsInList(linkedList1);
+  cout << "Ingrese una lista incluyendo el orden y los nodos de la lista: " << endl;
+  getline(cin, stringToken);
+  linkedList1.push_back(stringToken);
 
-  humanTree->preordenInorden(linkedList1, linkedList2);
+  cout << "Ingrese una segunda lista incluyendo el orden diferente al anterior y los nodos de la lista: " << endl;
+  getline(cin, stringToken);
+  linkedList2.push_back(stringToken);
 
-  cout << humanTree->peso() << endl;
 }
 
+void saveData(list<string> &linkedList) {
+  int numberTestCase;
+  string testCase;
 
-template <typename T>
-void getDataToInsertInTree(list<T> &linkedList, int numberTestCase) {
-  string inorden, preorden, postorden;
-  string inordenChar="INORDEN", preordenChar="PREORDEN", postordenChar="POSTORDEN";
-  list<T> list1, list2;
+  readData(testCase, "el numero de casos de prueba");
+  linkedList.push_back(testCase);
 
-  inorden = inordenChar.length();
-  preorden = preordenChar.length();
-  postorden = postordenChar.length();
+  char testCaseChar;
+  testCase[testCase.size()];
+  numberTestCase = atoi(testCaseChar);
 
-  for (int iterator = 0; iterator < linkedList.size(); ++iterator) {
-    string dataFront = linkedList.front();
+  // list<string> linkedList1, linkedList2;
+  // for (int i = 0; i < 2; ++i) {
+  //   string pointInitial, pointEnding;
 
-    if (iterator == 0) {
-      list1.push_back(dataFront);
-    }
-    else if (iterator == 1) {
-      list2.push_back(dataFront);
-    } else {
-      linkedList.pop_front();
-    }
-    linkedList.pop_front();
-  }
+  //   readData(pointInitial, "el punto de partida del recorrido");
+  //   linkedList.push_back(pointInitial);
 
-  setTree(list1, list2);
+  //   readData(pointEnding, "el punto de final del recorrido");
+  //   linkedList.push_back(pointEnding);
+
+  //   saveDataInList(linkedList1, linkedList2);
+  //   linkedList.merge(linkedList1);
+  //   linkedList.merge(linkedList2);
+  //   linkedList.push_back(" ");
+  // }
 }
 
 int main() {
-  list<string> dataList;
-  readFileToSaveOnList(dataList);
+  list<string> linkedListFileIn, linkedListFileOut;
 
-  int numberTestCase;
-  numberTestCase = stoi(dataList.front());
-  dataList.pop_front();
+  saveData(linkedListFileIn);
 
-  getDataInitialAndEnd(dataList);
-  getDataToInsertInTree(dataList, numberTestCase);
+
+
 
   return 0;
 }
+
+
+
+// string nodeInitial, nodeEnding;
+// list<string> linkedFinal;
+
+// template <typename T>
+// void getDataInitialAndEnd(list<T> &linkedList) {
+//   int amountChar;
+//   string dataPerson;
+//   string delimiter= "-";
+//   string dataInitialAndEnd[1];
+
+//   string words;
+//   words = linkedList.front();
+//   linkedList.pop_front();
+
+//   int iteratorMaxPerson=0;
+//   while (((amountChar = words.find(delimiter)) != string::npos) && iteratorMaxPerson < 2) {
+//     dataPerson = words.substr(0, amountChar+2);
+//     words.erase(0, amountChar + delimiter.length() + 2);
+
+//     dataInitialAndEnd[iteratorMaxPerson] = dataPerson;
+//     iteratorMaxPerson++;
+//   }
+
+//   nodeInitial = dataInitialAndEnd[0];
+//   nodeEnding = dataInitialAndEnd[1];
+// }
+
+// template <typename T>
+// void setTree(list<T> &linkedList1, list<T> &linkedList2) {
+//   ArbolBinario<string> *humanTree = new ArbolBinario<string>;
+//   list<string> lista;
+
+//   showElementsInList(linkedList1);
+
+//   humanTree->preordenInorden(linkedList1, linkedList2);
+
+//   cout << humanTree->peso() << endl;
+// }
+
+
+// template <typename T>
+// void getDataToInsertInTree(list<T> &linkedList, int numberTestCase) {
+//   string inorden, preorden, postorden;
+//   string inordenChar="INORDEN", preordenChar="PREORDEN", postordenChar="POSTORDEN";
+//   list<T> list1, list2;
+
+//   inorden = inordenChar.length();
+//   preorden = preordenChar.length();
+//   postorden = postordenChar.length();
+
+//   for (int iterator = 0; iterator < linkedList.size(); ++iterator) {
+//     string dataFront = linkedList.front();
+
+//     if (iterator == 0) {
+//       list1.push_back(dataFront);
+//     }
+//     else if (iterator == 1) {
+//       list2.push_back(dataFront);
+//     } else {
+//       linkedList.pop_front();
+//     }
+//     linkedList.pop_front();
+//   }
+
+//   setTree(list1, list2);
+// }
+
+
+
+// int main() {
+//   list<string> dataList;
+//   readFileToSaveOnList(dataList);
+
+//   int numberTestCase;
+//   numberTestCase = stoi(dataList.front());
+//   dataList.pop_front();
+
+//   getDataInitialAndEnd(dataList);
+//   getDataToInsertInTree(dataList, numberTestCase);
+
+//   return 0;
+// }
