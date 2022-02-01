7b0ac3e1abfce5d51e5a0ea35ea254461ea9cf5b
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2022-02-01 00:10:22 -0400

    wip 2: pasando a lo que me pasaron

--- main.cpp
+++ main.cpp
@@ -6,63 +6,108 @@
 
 using namespace std;
 
-template <typename T>
-void readData(T &data, string info) {
-  cout << "Ingrese " << info << " : " << endl;
-  cin >> data;
+void extraerEntradas(string cadenaBase, string& a, string& b) {
+  int posSeparacion = cadenaBase.find(" ");  // enconrtamos el espacio que separa los casos
+  a = cadenaBase.substr(0,posSeparacion); // extraemos a y b para hallar el recorrido
+  b = cadenaBase.substr(posSeparacion + 1);
 }
 
-void saveDataInList(list<string> &linkedList1, list<string> &linkedList2) {
-  string stringToken;
+void extraerLista(string cadenaDeLista, list<string> &listaParaArbol, string &modoDeCreacion) {
+  int contEspacio = cadenaDeLista.find(" "); // con esto conseguimos el modo de creacion, y luego los otros espacios por elemento
+  int siguienteElemento = 0;
 
-  cout << "Ingrese una lista incluyendo el orden y los nodos de la lista: " << endl;
-  getline(cin, stringToken);
-  linkedList1.push_back(stringToken);
-
-  cout << "Ingrese una segunda lista incluyendo el orden diferente al anterior y los nodos de la lista: " << endl;
-  getline(cin, stringToken);
-  linkedList2.push_back(stringToken);
-
+  modoDeCreacion = cadenaDeLista.substr(0, contEspacio); // obtenemos  PREORDEN, INORDEN o POSTORDEN
+  siguienteElemento = contEspacio + 1; // en terminos de la cadena pasamos al caracter despues del espacio
+  contEspacio = cadenaDeLista.find(" ", siguienteElemento);
+  //cout << cadenaDeLista.at(siguienteElemento) << contEspacio;
+  while(contEspacio != -1) {
+    //cout << "(" << cadenaDeLista.substr(siguienteElemento, siguienteElemento + contEspacio) << ")";
+    listaParaArbol.push_back(cadenaDeLista.substr(siguienteElemento, contEspacio - siguienteElemento));// obtenemos un elemento de la lista
+    siguienteElemento = contEspacio + 1; // pasamos al caracter despues del espacio
+    contEspacio = cadenaDeLista.find(" ", siguienteElemento); // leemos el proximo espacio
+  }
 }
 
-void saveData(list<string> &linkedList) {
-  int numberTestCase;
-  string testCase;
-
-  readData(testCase, "el numero de casos de prueba");
-  linkedList.push_back(testCase);
-
-  char testCaseChar;
-  testCase[testCase.size()];
-  numberTestCase = atoi(testCaseChar);
-
-  // list<string> linkedList1, linkedList2;
-  // for (int i = 0; i < 2; ++i) {
-  //   string pointInitial, pointEnding;
-
-  //   readData(pointInitial, "el punto de partida del recorrido");
-  //   linkedList.push_back(pointInitial);
-
-  //   readData(pointEnding, "el punto de final del recorrido");
-  //   linkedList.push_back(pointEnding);
-
-  //   saveDataInList(linkedList1, linkedList2);
-  //   linkedList.merge(linkedList1);
-  //   linkedList.merge(linkedList2);
-  //   linkedList.push_back(" ");
-  // }
-}
 
 int main() {
-  list<string> linkedListFileIn, linkedListFileOut;
+  int casos, contador;
+  string entrada, listas; // entrada para elemento, entrada para listas
+  string a, b, aux, modoDeCreacion;
+  list<string> listaPrePost, listaIn;
 
-  saveData(linkedListFileIn);
+  cin >> casos;
+  cin.ignore();
 
-
-
-
+  for (contador = 1; contador <= casos; contador++) {
+    getline(cin, entrada); // leo elementos a y b;
+    extraerEntradas(entrada, a, b); // asigno a y b a variables
+    getline(cin, listas); // leo la lista 1;
+    extraerLista(listas, listaPrePost, aux); // Extraigo la lista 1
+    modoDeCreacion = aux; // aquí se asigna como se construye el arbol
+    getline(cin, entrada); // leo la lista 2 (ultima linea de entrada)
+    extraerLista(listas, listaInorden, aux); // ahora extraemos la lista inorden , ya no nos interesa el modo
+    getline(cin, entrada); // aqui se lee el espacio entre casos
+    //cout << a << " "<< b << " " << aux <<  " " << listaPrePost.front() << " " << listaPrePost.back();  // PARA TESTEAR
+  }
   return 0;
 }
+
+// void readData(T &data, string info) {
+//   cout << "Ingrese " << info << " : " << endl;
+//   cin >> data;
+// }
+
+// void saveDataInList(list<string> &linkedList1, list<string> &linkedList2) {
+//   string stringToken;
+
+//   cout << "Ingrese una lista incluyendo el orden y los nodos de la lista: " << endl;
+//   getline(cin, stringToken);
+//   linkedList1.push_back(stringToken);
+
+//   cout << "Ingrese una segunda lista incluyendo el orden diferente al anterior y los nodos de la lista: " << endl;
+//   getline(cin, stringToken);
+//   linkedList2.push_back(stringToken);
+
+// }
+
+// void saveData(list<string> &linkedList) {
+//   int numberTestCase;
+//   string testCase;
+
+//   readData(testCase, "el numero de casos de prueba");
+//   linkedList.push_back(testCase);
+
+//   char testCaseChar;
+//   testCase[testCase.size()];
+//   numberTestCase = atoi(testCaseChar);
+
+//   // list<string> linkedList1, linkedList2;
+//   // for (int i = 0; i < 2; ++i) {
+//   //   string pointInitial, pointEnding;
+
+//   //   readData(pointInitial, "el punto de partida del recorrido");
+//   //   linkedList.push_back(pointInitial);
+
+//   //   readData(pointEnding, "el punto de final del recorrido");
+//   //   linkedList.push_back(pointEnding);
+
+//   //   saveDataInList(linkedList1, linkedList2);
+//   //   linkedList.merge(linkedList1);
+//   //   linkedList.merge(linkedList2);
+//   //   linkedList.push_back(" ");
+//   // }
+// }
+
+// int main() {
+//   list<string> linkedListFileIn, linkedListFileOut;
+
+//   saveData(linkedListFileIn);
+
+
+
+
+//   return 0;
+// }
 
 
 
